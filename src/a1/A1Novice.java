8a251f008f95787ff079713699d9dbd8144346be
SylviaSzarka <SylviaSzarka@sylvias-mbp.lan1>
2019-09-03 01:23:08 -0400

    commit

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -1,3 +1,6 @@
+//For 	int[] values = new int[count];this assignment, you shouldn't have to use any Scanner methods other than next(), nextInt(), and nextDouble().
+//In particular, you should avoid using nextLine(). 
+// do not worry about validating input or being able to deal w/ unexpected input
 package a1;
 
 import java.util.Scanner;
@@ -7,8 +10,38 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+	// Your code follows here.
 		
+		int numOfCustomers = scan.nextInt();
+		
+		String[] customerFirst = new String[numOfCustomers];	
+		String[] customerSecond = new String[numOfCustomers];
+		int[] customerItemsNum = new int[numOfCustomers];
+		double[] customerTotals = new double[numOfCustomers];
+		
+		
+		
+		
+		for(int i=0; i < numOfCustomers; i++) {
+			customerFirst[i] = scan.next();
+			customerSecond[i] = scan.next();		
+			customerItemsNum[i] = scan.nextInt();
+			System.out.println(customerFirst[i].charAt(0) + "." + " " + customerSecond[i] + " " + customerItemsNum[i]);
+				
+			for(int j=0; j < customerItemsNum[i]; j++) {
+				int amount = scan.nextInt();
+				String item = scan.next();
+				double cost = scan.nextDouble();
+				customerTotals[i] += amount*cost;
+			}
+			System.out.println(customerTotals[i]);
+		}
+		scan.close();
+		}
+			
 	}
-}
+		
+
+
+
+
